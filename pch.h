8bd57c908fdeaf1ae19ef635339390a1aa80d4b5
Spectral <88994020+Cvahotni@users.noreply.github.com>
2026-06-16 11:18:17 +1000

    Create pch.h

---
+++ pch.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <thread>
+#include <utility>
+#include <memory>
+#include <random>
+#include <chrono>
+#include <atomic>
+#include <mutex>
+#include <filesystem>
+#include <variant>
+#include <condition_variable>
+
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <bitset>
+#include <execution>
+
+#include <cstddef>
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <cctype>
+
+#include <fstream>
+#include <sstream>
