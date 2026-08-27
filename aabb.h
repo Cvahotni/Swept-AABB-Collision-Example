f20d85981f94dd1d7f1e2363d718d1ee3c7f165f
Spectral <88994020+Cvahotni@users.noreply.github.com>
2026-08-27 15:49:46 +1000

    Update aabb.h

--- aabb.h
+++ aabb.h
@@ -19,6 +19,14 @@ public:
         );
     }
 
+    bool PointIntersectsAlternate(Vector3 alternatePos, AABB other) {
+        return (
+            alternatePos.X() > other.min.X() && alternatePos.X() < other.max.X() && 
+            alternatePos.Y() > other.min.Y() && alternatePos.Y() < other.max.Y() && 
+            alternatePos.Z() > other.min.Z() && alternatePos.Z() < other.max.Z()
+        );
+    }
+
     bool IntersectsBlock(int32_t blockX, int32_t blockY, int32_t blockZ) {
         Vector3 blockMin(blockX, blockY, blockZ);
         Vector3 blockMax(blockX + 1, blockY + 1, blockZ + 1);
@@ -44,23 +52,53 @@ public:
         double t5 = (aabb.min.Z() - origin.Z()) * dirFraction[2];
         double t6 = (aabb.max.Z() - origin.Z()) * dirFraction[2];
 
-        double tmin = std::max(
+        double tMin = std::max(
             std::max(std::min(t1, t2), std::min(t3, t4)
         ), std::min(t5, t6));
 
-        double tmax = std::min(
+        double tMax = std::min(
             std::min(std::max(t1, t2), std::max(t3, t4)
         ), std::max(t5, t6));
 
-        if(tmax < 0) {
+        if(tMax < 0) {
             return false;
         }
 
-        if(tmin > tmax) {
+        if(tMin > tMax) {
             return false;
         }
         
         return true;
+    }
+
+    bool AnyPointOfAABB(AABB& b) {
+        Vector3 vertices[8] = {
+            Vector3{min.X(), min.Y(), min.Z()},
+            Vector3{max.X(), min.Y(), min.Z()},
+            Vector3{min.X(), max.Y(), min.Z()},
+            Vector3{min.X(), min.Y(), max.Z()},
+            Vector3{max.X(), max.Y(), min.Z()},
+            Vector3{max.X(), min.Y(), max.Z()},
+            Vector3{min.X(), max.Y(), max.Z()},
+            Vector3{max.X(), max.Y(), max.Z()}
+        };
+
+        for(auto& vertex : vertices) {
+            if(PointIntersectsAlternate(vertex, b)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    std::string ToString() {
+        return (
+            min.ToString() + ", " +
+            max.ToString() + ", " +
+            pos.ToString() + ", " +
+            size.ToString()
+        );
     }
 
     Vector3 min{};
